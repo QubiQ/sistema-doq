177aab41668d8315f4780e4df3d3480ad21f53be
Jesus Ramoneda <jesus.ramoneda@qubiq.es>
2022-03-15 11:53:00 +0100

    Upload Exercise

---
+++ practica_final_3/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Práctica Final 3",
+    "summary": "Práctica Final 3 Sistema DOQ Base",
+    "version": "14.0.1.0.0",
+    "category": "Sistema DOQ",
+    "website": "https://www.qubiq.es",
+    "author": "QubiQ",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [],
+    "data": ['security/ir.model.access.csv',
+             'data/menu.xml',
+             'views/res_partner.xml',
+             'views/books_book.xml',
+             'views/books_author.xml',
+             'views/report_books.xml',
+             'views/reports.xml',
+             'views/books_genre.xml'],
+}
